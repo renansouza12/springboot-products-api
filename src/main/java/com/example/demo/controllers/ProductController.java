eee8c4444fc1b0e64c13e69c86dd3ce250445c59
Renan <renansouza0122@gmail.com>
2024-08-17 18:09:17 -0400

    :sparkles: feat: method get one product

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.demo.dtos.ProductRecordDTO;
 import com.example.demo.models.ProductModel;
@@ -28,12 +32,21 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
         var productModel = new ProductModel();
-        BeanUtils.copyProperties(productRecordDTO, productModel);   
+        BeanUtils.copyProperties(productRecordDTO, productModel);       
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
     @GetMapping
     public ResponseEntity<List<ProductModel>> getAllProducts(){
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable UUID id){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if(product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
 }
